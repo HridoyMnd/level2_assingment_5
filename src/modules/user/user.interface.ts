cfda28bbaa3a429e681034c17945ccf855421fec
Hridoy Mnd <hridoymnd10@gmail.com>
2025-07-30 16:57:17 +0600

    user interface and model create done

---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,39 @@
+
+import { Types } from "mongoose";
+
+// IAuthProvider interface
+export interface IAuthProvider {
+    provider: "google" | "credentials",
+    providerId: string
+}
+
+// user status
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED"
+}
+
+// user role
+export enum UserRole {
+  USER = "USER",
+  AGENT = "AGENT",
+  ADMIN = "ADMIN"
+}
+
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?:string,
+  address?: string;
+  isDeleted?:boolean;
+  isVerified?:boolean;
+  isActive?:IsActive;
+  role?: UserRole;
+  auths: IAuthProvider[];
+  wallet?:Types.ObjectId;
+
+}
